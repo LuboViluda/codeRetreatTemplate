f0d754f3adfd52bc8ff9765176866745866cc4b0
Lubo Viluda <lubo.viluda@gmail.com>
2017-07-13 16:47:26 +0200

    empty main class and test class

---
+++ src/main/java/GameOfLife.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lubomir.viluda on 7/13/2017.
+ */
+public class GameOfLife {
+    public static void main(String[] args) {
+        System.out.println("Hello Code retreat");
+    }
+}
---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by lubomir.viluda on 7/13/2017.
+ */
+public class GameOfLifeTest {
+
+    @Test
+    public void sum_twoPositiveNumbers() throws Exception {
+        assertEquals(5, 5);
+    }
+}
